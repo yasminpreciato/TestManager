09fd782f85881bbde96eb505b2d85d4bf28d2aa2
yasminpreciato <yasminfpreciato2019@gmail.com>
2025-02-24 20:33:20 -0300

    cria o cabeçalho da tela taskList

--- src/screens/Tasklist.js
+++ src/screens/Tasklist.js
@@ -1,10 +1,16 @@
 import { View, Text, ImageBackground, StyleSheet} from "react-native"
 
+import todayImage from '../../assets/imgs/today.jpg'
 
 export default function Tasklist() {
     return(
         <View style={styles.container}>
-            <ImageBackground source={{}} style={StyleSheet.background}>
+            <ImageBackground source={todayImage} style={styles.background}>
+
+                <View style={styles.titleBar}>
+                    <Text style={styles.title}>Hoje</Text>
+                    <Text style={styles.subtitle}>24 de Fevereiro</Text>
+                </View>
 
             </ImageBackground>
             <View style={styles.tasklist}>
@@ -20,11 +26,28 @@ const styles = StyleSheet.create({
         flex: 1
     },
     background:{
-        flex: 3,
-        backgroundColor: "blue"
+        flex: 3
     },
     tasklist:{
         flex: 7,
-        backgroundColor: "pink"
+        backgroundColor: "#F5DEB3"
+    },
+    titleBar:{
+        flex: 1,
+        justifyContent:'flex-end'
+
+    },
+    title: {
+        color: 'white',
+        fontSize: 50, 
+        marginLeft: 20,
+        marginBottom: 20
+
+    },
+    subtitle: {
+        color: 'white',
+        fontSize: 20,
+        marginLeft: 20,
+        marginBottom: 30
     }
 })
