fa552912197768362ff95e36a373309f151b3737
yasminpreciato <yasminfpreciato2019@gmail.com>
2025-05-12 20:03:00 -0300

    Implementa um componente Data na tela Addtask

--- src/screens/AddTask.js
+++ src/screens/AddTask.js
@@ -1,73 +1,76 @@
-import { Modal, TouchableWithoutFeedback, View, StyleSheet, Text, TextInput, TouchableOpacity} from "react-native";
-import { useState} from "react";
+import { Modal, TouchableWithoutFeedback, View, StyleSheet, Text, TextInput, TouchableOpacity, Platform } from "react-native";
+import { useState } from "react";
 
 import DateTimePicker from "@react-native-community/datetimepicker";
 import moment from "moment";
 
-export default function AddTask(props){
+export default function AddTask(props) {
 
     const [desc, setDesc] = useState("")
     const [date, setDate] = useState(new Date())
     const [showDatePicker, setShowDatePicker] = useState(false)
 
     const handleDateChange = (event, selectedDate) => {
-        if (selectedDate){
+        setShowDatePicker(Platform.OS === 'ios')
+        if (selectedDate) {
             setDate(selectedDate)
         }
     }
 
     const formattedDate = moment(date).format('ddd, D [de] MMMM [de] YYYY')
 
-    return(
-       <Modal transparent={true} 
-       visible={props.isVisible} 
-       onRequestClose={props.onCancel}
-       animationType="slide">
+    return (
+        <Modal transparent={true}
+            visible={props.isVisible}
+            onRequestClose={props.onCancel}
+            animationType="slide">
 
-        <TouchableWithoutFeedback
-            onPress={props.onCancel}>
-            <View style={styles.background}></View>
-        </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback
+                onPress={props.onCancel}>
+                <View style={styles.background}></View>
+            </TouchableWithoutFeedback>
 
-        <View style={styles.container}>
-            <Text style={styles.header}> Nova Tarefa </Text>
-            <TextInput
-                style={styles.input}
-                placeholder="Informe a descrição"
-                onChange={setDesc}
-                value={desc} />
+            <View style={styles.container}>
+                <Text style={styles.header}> Nova Tarefa </Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Informe a descrição"
+                    onChange={setDesc}
+                    value={desc} />
 
-            <View>
-                <TouchableOpacity onPress={() => setShowDatePicker(true)}>
-                    <Text style={styles.date}>{formattedDate}</Text>
-                </TouchableOpacity>
-                {showDatePicker && (
-                    <DateTimePicker
-                        value={date}
-                        mode="date"
-                        display="default"
-                        onChange={handleDateChange}
-                    />
+                {Platform.OS === 'android' && (
+                    <View>
+                        <TouchableOpacity onPress={() => setShowDatePicker(true)}>
+                            <Text style={styles.date}>{formattedDate}</Text>
+                        </TouchableOpacity>
+                        {showDatePicker && (
+                            <DateTimePicker
+                                value={date}
+                                mode="date"
+                                display="default"
+                                onChange={handleDateChange}
+                            />
+                        )}
+                    </View>
                 )}
-            </View>    
 
-            <View style={styles.buttons}>
-                <TouchableOpacity onPress={props.onCancel}>
-                    <Text style={styles.button}>Cancelar</Text>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text style={styles.button}>Salvar</Text>
-                </TouchableOpacity>
+                <View style={styles.buttons}>
+                    <TouchableOpacity onPress={props.onCancel}>
+                        <Text style={styles.button}>Cancelar</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Text style={styles.button}>Salvar</Text>
+                    </TouchableOpacity>
+                </View>
+
             </View>
 
-        </View>
+            <TouchableWithoutFeedback
+                onPress={props.onCancel}>
+                <View style={styles.background}></View>
+            </TouchableWithoutFeedback>
 
-        <TouchableWithoutFeedback
-            onPress={props.onCancel}>
-            <View style={styles.background}></View>
-        </TouchableWithoutFeedback>
-
-       </Modal> 
+        </Modal>
     )
 }
 
@@ -100,6 +103,11 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-end'
     },
     button: {
+        margin: 20,
+        marginRight: 30,
+        color: '#b13b44'
+    },
+    date: {
         margin: 20,
         marginRight: 30,
         color: '#b13b44'
